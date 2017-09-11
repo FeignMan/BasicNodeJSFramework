2119d64ba459321e5e3d916dc1654f8a0235d36f
Ishan Aditya <ishan.aditya@ubisoft.com>
2017-09-11 15:44:16 +0200

    updated requestHandler to collect request body

--- app.js
+++ app.js
@@ -11,8 +11,29 @@ var videoiFrame = "<p><iframe width=\"560\" height=\"315\" src=\"https://www.you
 //	Before we create a HTTP server, let's create a request handler.
 //	The request handler is called once for every HTTP request made against the server.
 var requestHandler = function (request, response) {
-	log.info(request.method, "request received from:", request.connection.remoteAddress, "URL Requested:", request.url);
-	// console.log(request.headers);
+	log.info(request.method, "request received from:", request.connection.remoteAddress, "URL:", request.url);
+	log.info("Request Headers:", request.headers);
+
+	var body = "";
+	request.on("error", function(err) {
+		log.error("Error from the request stream:", err);
+		request.abort();
+	}).on("data", function(data) {
+		log.info("Received data chunk:", data);
+		body += data;
+
+		//	Don't let yourself run out of RAM!
+		// if (body.length > 1e6)
+		// 	request.connection.destroy();
+
+	}).on("end", function() {
+		if (body.length > 0) log.info("Request Body:", body);
+	});
+
+	response.on("error", function(err) {
+		log.error("Error from the response stream:", err);
+		response.abort();
+	});
 
 	//	Write response status code, and response headers
 	//	If not specified, status code defaults to 200. Can be specified, eg. response.statusCode = 404
@@ -47,7 +68,7 @@ server.on("error", function(err) {
 		log.info("Shutting down...");
 		process.exit(1);
 	});
-})
+});
 
 server.listen(3000, function(err) {
 	if (err) {
